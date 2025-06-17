e15bca83c5ab79c1bdfad8c9f8585cf4da764275
076189 <209686481+076189@users.noreply.github.com>
2025-06-17 20:39:19 +0100

    Update sw-omsi.js

--- sw-omsi.js
+++ sw-omsi.js
@@ -1,1 +1,9 @@
-
+const CACHE_NAME = 'omsi-arrivals-cache-v5'; // Increment this again for sure!
+const urlsToCache = [
+  '/live-tracker/omsi_arrivals.html',
+  '/live-tracker/manifest-omsi.json',
+  '/live-tracker/offline.html',
+  '/live-tracker/fonts/NJFont-Medium.ttf',
+  '/live-tracker/assets/icons/icon-192x192.png',
+  '/live-tracker/assets/icons/icon-512x512.png'
+];

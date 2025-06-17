e6431a06e806a8c9ad5e37c4fddb844295970c86
076189 <209686481+076189@users.noreply.github.com>
2025-06-17 20:58:30 +0100

    Update sw-omsi.js

--- sw-omsi.js
+++ sw-omsi.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'omsi-arrivals-cache-v5'; // Increment this again for sure!
+const CACHE_NAME = 'omsi-arrivals-cache-v6';
 const urlsToCache = [
-  '/live-tracker/omsi_arrivals.html',
-  '/live-tracker/manifest-omsi.json',
-  '/live-tracker/offline.html',
-  '/live-tracker/fonts/NJFont-Medium.ttf',
-  '/live-tracker/assets/icons/icon-192x192.png',
-  '/live-tracker/assets/icons/icon-512x512.png'
+  'https://076189.github.io/live-tracker/omsi_arrivals.html',
+  'https://076189.github.io/live-tracker/manifest-omsi.json',
+  'https://076189.github.io/live-tracker/offline.html',
+  'https://076189.github.io/live-tracker/fonts/NJFont-Medium.ttf',
+  'https://076189.github.io/live-tracker/assets/icons/icon-192x192.png',
+  'https://076189.github.io/live-tracker/assets/icons/icon-512x512.png'
 ]

d1c53255e00b64dd13cdd961fa4b754ee284412c
076189 <209686481+076189@users.noreply.github.com>
2026-09-25 00:35:57 +0100

    Add files via upload

---
+++ sw-bus_fleet_generator.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'bus_fleet_generator';
+const urlsToCache = [
+  './bus_fleet_generator.html',
+  './manifest-bus_fleet_generator.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
